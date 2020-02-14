a549f4731c924605df29569c89fc20976153f525
Jan Prach <jendap@gmail.com>
2020-02-13 23:27:19 -0800

    revamp Arbitrary
    * add default and proto strategies
    * share more code between strategies
    * clamp temporal values properly
    * switch distributions to Long
    * add tests for java.time package

--- core/src/main/scala/com/nuna/trustdb/core/util/Arbitrary.scala
+++ core/src/main/scala/com/nuna/trustdb/core/util/Arbitrary.scala
@@ -4,7 +4,8 @@ import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 import java.time._
-import java.util.concurrent.atomic.AtomicInteger
+import java.time.temporal.Temporal
+import java.util.concurrent.atomic.AtomicLong
 
 import scala.reflect.runtime.universe._
 import scala.util.Random
@@ -20,19 +21,21 @@ import scala.util.Random
 //   - Arbitrary.hashed[T] - (salted) hash of field name (better than random ;-)
 //
 // Note: It supports only some basic types - the kind of stuff all serialization frameworks do support.
-class Arbitrary(strategy: Arbitrary.ArbitraryStrategy.Value, seed: Int = 0) {
+class Arbitrary(strategy: Arbitrary.ArbitraryStrategy.Value, seed: Long = 0) {
   import Arbitrary._
 
-  private val counter = new AtomicInteger(seed)
+  private val counter = new AtomicLong(seed)
 
   def make[T <: Product : TypeTag]: T = {
     strategy match {
       case ArbitraryStrategy.CONSTANT => Arbitrary.constant[T](seed)
+      case ArbitraryStrategy.DEFAULT => Arbitrary.default[T]
       case ArbitraryStrategy.EMPTY => Arbitrary.empty[T]
       case ArbitraryStrategy.HASHED => Arbitrary.hashed[T](counter.getAndIncrement())
       case ArbitraryStrategy.HASHED_CONSTANT => Arbitrary.hashed[T](seed)
       case ArbitraryStrategy.INDEXED => Arbitrary.indexed[T](counter.getAndAdd(1000))
       case ArbitraryStrategy.INDEXED_CONSTANT => Arbitrary.constant[T](counter.getAndIncrement())
+      case ArbitraryStrategy.PROTO => Arbitrary.proto[T]
       case ArbitraryStrategy.RANDOM => Arbitrary.random[T](counter.getAndIncrement())
     }
   }
@@ -40,62 +43,67 @@ class Arbitrary(strategy: Arbitrary.ArbitraryStrategy.Value, seed: Int = 0) {
 
 object Arbitrary {
   object ArbitraryStrategy extends Enumeration {
-    val CONSTANT, EMPTY, HASHED, HASHED_CONSTANT, INDEXED, INDEXED_CONSTANT, RANDOM = Value
+    val CONSTANT, DEFAULT, EMPTY, HASHED, HASHED_CONSTANT, INDEXED, INDEXED_CONSTANT, PROTO, RANDOM = Value
   }
 
-  trait NaiveIntegerDistribution {
-    def sample(param: Symbol): Int
+  private val EPOCH_DAY_MIN = LocalDate.of(1, 1, 1).toEpochDay
+  private val EPOCH_DAY_MAX = LocalDate.of(9999, 12, 31).toEpochDay
+  private val SECOND_OF_DAY_MIN = LocalTime.of(0, 0, 0).toSecondOfDay
+  private val SECOND_OF_DAY_MAX = LocalTime.of(23, 59, 59).toSecondOfDay
+
+  trait NaiveDistribution {
+    def sample(param: Symbol): Long
   }
 
-  class ConstantDistribution(constant: Int) extends NaiveIntegerDistribution {
-    override def sample(param: Symbol): Int = constant
+  class ConstantDistribution(constant: Long) extends NaiveDistribution {
+    override def sample(param: Symbol): Long = constant
   }
 
-  class CounterDistribution(counter: AtomicInteger) extends NaiveIntegerDistribution {
-    override def sample(param: Symbol): Int = counter.getAndIncrement()
+  class CounterDistribution(counter: AtomicLong) extends NaiveDistribution {
+    override def sample(param: Symbol): Long = counter.getAndIncrement()
   }
 
-  class RandomDistribution(seed: Int) extends NaiveIntegerDistribution {
+  class RandomDistribution(seed: Long) extends NaiveDistribution {
     private val random = new Random(seed)
 
-    override def sample(param: Symbol): Int = random.nextInt()
+    override def sample(param: Symbol): Long = random.nextLong()
   }
 
-  class HashedDistribution(salt: Int) extends NaiveIntegerDistribution {
+  class HashedDistribution(salt: Long) extends NaiveDistribution {
     private val messageDigest = MessageDigest.getInstance("SHA-1")
 
-    override def sample(param: Symbol): Int = {
+    override def sample(param: Symbol): Long = {
       val paramName = param.name.decodedName
       val saltedParamName = s"$paramName.$salt"
       val digest = messageDigest.digest(saltedParamName.getBytes(StandardCharsets.UTF_8))
-      ByteBuffer.wrap(digest).getInt
+      ByteBuffer.wrap(digest).getLong
     }
   }
 
-  def constant[T <: Product : TypeTag](value: Int): T = {
+  def constant[T <: Product : TypeTag](value: Long): T = {
     make[T](new ConstantDistribution(value))
+  }
+
+  def default[T <: Product : TypeTag]: T = {
+    val args = Reflection.getCaseClassFields[T].map { param =>
+      param.typeSignature.dealias match {
+        case t if t <:< typeOf[AnyVal] => makePrimitive(t, 0)
+        case t if Reflection.isCaseClass(t) => default(Reflection.typeToTypeTag(t))
+        case _ => null
+      }
+    }
+    Reflection.createCaseClassInstance[T](args)
   }
 
   def empty[T <: Product : TypeTag]: T = {
     val args = Reflection.getCaseClassFields[T].map { param =>
       param.typeSignature.dealias match {
+        case t if t <:< typeOf[AnyVal] => makePrimitive(t, 0)
+        case t if t =:= typeOf[String] => null
+        case t if t <:< typeOf[Enumeration#Value] => makeEnumerationValue(0)(Reflection.typeToTypeTag(t))
         case t if t <:< typeOf[Option[_]] => Option.empty
-        case t if t <:< typeOf[Set[_]] => Set.empty
-        case t if t <:< typeOf[Seq[_]] => Seq.empty
-        case t if t <:< typeOf[Map[_, _]] => Map.empty
-        case t if t <:< typeOf[Iterable[_]] => Iterable.empty
-
-        case t if t <:< typeOf[Enumeration#Value] => chooseEnumValue(0)(Reflection.typeToTypeTag(t))
-
-        case t if t =:= typeOf[Boolean] => false
-        case t if t =:= typeOf[Byte] => 0.toByte
-        case t if t =:= typeOf[Short] => 0.toShort
-        case t if t =:= typeOf[Char] => 0.toChar
-        case t if t =:= typeOf[Int] => 0
-        case t if t =:= typeOf[Long] => 0.toLong
-        case t if t =:= typeOf[Float] => 0.toFloat
-        case t if t =:= typeOf[Double] => 0.toDouble
-
+        case t if t <:< typeOf[Iterable[_]] => makeEmptyCollection(t)
+        case t if t <:< typeOf[Temporal] || t <:< typeOf[java.util.Date] => null
         case t if Reflection.isCaseClass(t) => empty(Reflection.typeToTypeTag(t))
         case _ => null
       }
@@ -103,48 +111,45 @@ object Arbitrary {
     Reflection.createCaseClassInstance[T](args)
   }
 
-  def indexed[T <: Product : TypeTag](from: Int): T = {
-    val counter = new AtomicInteger(from)
-    make[T](new CounterDistribution(counter))
-  }
-
-  def random[T <: Product : TypeTag](seed: Int): T = {
-    make[T](new RandomDistribution(seed))
-  }
-
-  def hashed[T <: Product : TypeTag](salt: Int): T = {
+  def hashed[T <: Product : TypeTag](salt: Long): T = {
     make[T](new HashedDistribution(salt))
   }
 
-  def make[T <: Product : TypeTag](distribution: NaiveIntegerDistribution): T = {
+  def indexed[T <: Product : TypeTag](from: Long): T = {
+    val counter = new AtomicLong(from)
+    make[T](new CounterDistribution(counter))
+  }
+
+  def proto[T <: Product : TypeTag]: T = {
+    val args = Reflection.getCaseClassFields[T].map { param =>
+      param.typeSignature.dealias match {
+        case t if t <:< typeOf[AnyVal] => makePrimitive(t, 0)
+        case t if t =:= typeOf[String] => ""
+        case t if t <:< typeOf[Enumeration#Value] => makeEnumerationValue(0)(Reflection.typeToTypeTag(t))
+        case t if t <:< typeOf[Option[_]] => Option.empty
+        case t if t <:< typeOf[Iterable[_]] => makeEmptyCollection(t)
+        case t if t <:< typeOf[Temporal] || t <:< typeOf[java.util.Date] => null
+        case t if Reflection.isCaseClass(t) => proto(Reflection.typeToTypeTag(t))
+        case _ => null
+      }
+    }
+    Reflection.createCaseClassInstance[T](args)
+  }
+
+  def random[T <: Product : TypeTag](seed: Long): T = {
+    make[T](new RandomDistribution(seed))
+  }
+
+  def make[T <: Product : TypeTag](distribution: NaiveDistribution): T = {
     val args = Reflection.getCaseClassFields[T].map { param =>
       val number = distribution.sample(param)
       param.typeSignature.dealias match {
-        case t if t <:< typeOf[Option[_]] => Option.empty
-        case t if t <:< typeOf[Set[_]] => Set.empty
-        case t if t <:< typeOf[Seq[_]] => Seq.empty
-        case t if t <:< typeOf[Map[_, _]] => Map.empty
-        case t if t <:< typeOf[Iterable[_]] => Iterable.empty
-
-        case t if t <:< typeOf[Enumeration#Value] => chooseEnumValue(number)(Reflection.typeToTypeTag(t))
-
-        case t if t =:= typeOf[Boolean] => (number & 1) == 1
-        case t if t =:= typeOf[Byte] => number.toByte
-        case t if t =:= typeOf[Short] => number.toShort
-        case t if t =:= typeOf[Char] => number.toChar
-        case t if t =:= typeOf[Int] => number.toInt
-        case t if t =:= typeOf[Long] => number.toLong
-        case t if t =:= typeOf[Float] => number.toFloat
-        case t if t =:= typeOf[Double] => number.toDouble
+        case t if t <:< typeOf[AnyVal] => makePrimitive(t, number)
         case t if t =:= typeOf[String] => number.toString
-
-        case t if t =:= typeOf[LocalDate] => makeLocalDate(number)
-        case t if t =:= typeOf[LocalTime] => makeLocalTime(number)
-        case t if t =:= typeOf[LocalDateTime] => makeLocalDateTime(number)
-        case t if t =:= typeOf[Instant] => makeLocalDateTime(number).toInstant(ZoneOffset.UTC)
-        case t if t =:= typeOf[java.sql.Date] => java.sql.Date.valueOf(makeLocalDate(number))
-        case t if t =:= typeOf[java.sql.Timestamp] => java.sql.Timestamp.valueOf(makeLocalDateTime(number))
-
+        case t if t <:< typeOf[Enumeration#Value] => makeEnumerationValue(number)(Reflection.typeToTypeTag(t))
+        case t if t <:< typeOf[Option[_]] => Option.empty
+        case t if t <:< typeOf[Iterable[_]] => makeEmptyCollection(t)
+        case t if t <:< typeOf[Temporal] || t <:< typeOf[java.util.Date] => makeTemporal(t, number)
         case t if Reflection.isCaseClass(t) => make(distribution)(Reflection.typeToTypeTag(t))
         case t => throw new IllegalArgumentException(s"Generating type '$t' is not supported.")
       }
@@ -152,26 +157,65 @@ object Arbitrary {
     Reflection.createCaseClassInstance[T](args)
   }
 
-  private def chooseEnumValue[E <: Enumeration : TypeTag](index: Int): E#Value = {
+  private def makePrimitive(typ: Type, number: Long): AnyVal = {
+    typ match {
+      case t if t =:= typeOf[Boolean] => (number & 1) == 1
+      case t if t =:= typeOf[Byte] => number.toByte
+      case t if t =:= typeOf[Short] => number.toShort
+      case t if t =:= typeOf[Char] => number.toChar
+      case t if t =:= typeOf[Int] => number.toInt
+      case t if t =:= typeOf[Long] => number.toLong
+      case t if t =:= typeOf[Float] => number.toFloat
+      case t if t =:= typeOf[Double] => number.toDouble
+      case t => throw new IllegalArgumentException(s"Generating primitive of type '$t' is not supported.")
+    }
+  }
+
+  private def makeEnumerationValue[E <: Enumeration : TypeTag](number: Long): E#Value = {
     val values = Reflection.getEnumerationValues[E]
-    values(Math.abs(index) % values.size)
+    values(clamp(number, 0, values.size).toInt)
   }
 
-  private def makeLocalDate(day: Int): LocalDate = {
-    val minDay = LocalDate.of(0, 1, 1).toEpochDay
-    val maxDay = LocalDate.of(9999, 12, 31).toEpochDay
-    val adjustedDay = Math.abs(day - minDay) % maxDay + minDay
-    LocalDate.ofEpochDay(adjustedDay)
+  private def makeEmptyCollection(typ: Type): Any = {
+    typ match {
+      case t if t <:< typeOf[Set[_]] => Set.empty
+      case t if t <:< typeOf[Seq[_]] => Seq.empty
+      case t if t <:< typeOf[Map[_, _]] => Map.empty
+      case t if t <:< typeOf[Iterable[_]] => Iterable.empty
+      case t => throw new IllegalArgumentException(s"Generating collection of type '$t' is not supported.")
+    }
   }
 
-  private def makeLocalTime(second: Int): LocalTime = {
-    val minSecond = LocalTime.of(0, 0, 0).toSecondOfDay
-    val maxSecond = LocalTime.of(23, 59, 59).toSecondOfDay
-    val adjustedSecond = Math.abs(second - minSecond) % maxSecond + minSecond
-    LocalTime.ofSecondOfDay(adjustedSecond)
+  private def makeTemporal(typ: Type, number: Long): Any = {
+    typ match {
+      case t if t =:= typeOf[java.util.Date] =>
+        java.util.Date.from(java.sql.Date.valueOf(makeLocalDate(number)).toInstant)
+      case t if t =:= typeOf[java.sql.Date] => java.sql.Date.valueOf(makeLocalDate(number))
+      case t if t =:= typeOf[java.sql.Timestamp] => java.sql.Timestamp.valueOf(makeLocalDateTime(number))
+      case t if t =:= typeOf[LocalDate] => makeLocalDate(number)
+      case t if t =:= typeOf[LocalTime] => makeLocalTime(number)
+      case t if t =:= typeOf[LocalDateTime] => makeLocalDateTime(number)
+      case t if t =:= typeOf[Instant] => makeLocalDateTime(number).toInstant(ZoneOffset.UTC)
+      case t => throw new IllegalArgumentException(s"Generating temporal of type '$t' is not supported.")
+    }
   }
 
-  private def makeLocalDateTime(number: Int): LocalDateTime = {
+  private def makeLocalDate(day: Long): LocalDate = {
+    LocalDate.ofEpochDay(clamp(day, EPOCH_DAY_MIN, EPOCH_DAY_MAX))
+  }
+
+  private def makeLocalTime(second: Long): LocalTime = {
+    LocalTime.ofSecondOfDay(clamp(second, SECOND_OF_DAY_MIN, SECOND_OF_DAY_MAX))
+  }
+
+  private def makeLocalDateTime(number: Long): LocalDateTime = {
     LocalDateTime.of(makeLocalDate(number), makeLocalTime(number))
+  }
+
+  private[util] def clamp(value: Long, min: Long, max: Long): Long = {
+    val span = max - min + 1
+    val mod = (value - min) % span
+    val adjustedMod = if (mod < 0) mod + span else mod
+    adjustedMod + min
   }
 }
--- core/src/test/scala/com/nuna/trustdb/core/util/ArbitraryTest.scala
+++ core/src/test/scala/com/nuna/trustdb/core/util/ArbitraryTest.scala
@@ -2,31 +2,28 @@ package com.nuna.trustdb.core.util
 
 import java.time.{Instant, LocalDate, LocalDateTime, LocalTime}
 
-import com.nuna.trustdb.core.util.Arbitrary.ArbitraryStrategy
 import org.scalatest.funsuite.AnyFunSuite
 
 class ArbitraryTest extends AnyFunSuite {
+  import Arbitrary._
   import ArbitraryTest._
 
   test("Primitives") {
-    val expectedRandom42 = Primitives(true, -9, -17439, 205897768, 1325939940, -2.4879224E8f, 1.190043011E9)
-    val expectedHashed0 = Primitives(false, -42, 13843, 896151886, 722125446, -1.11864755E9f, -8.60392747E8)
-    val expectedHashed1 = Primitives(true, -66, 28179, -1200363285, 2037133044, 1.42971994E9f, -1.726633485E9)
+    val expectedHashed0 =
+      Primitives(true, -43, 18627, 1241660790, 3101505174803136092L, -4.8045547E18f, -3.6953587070444283E18)
+    val expectedHashed1 =
+      Primitives(false, 56, 12731, 996725495, 8749419802067486576L, 6.1406004E18f, -7.4158343478326579E18)
+    val expectedRandom42 =
+      Primitives(true, 40, -17589, -1255373459, -6169532649852302182L, -1.78246695E18f, 6.8028440265634191E18)
 
-    assert(Arbitrary.constant[Primitives](42) === Primitives(false, 42, 42, 42, 42L, 42.0f, 42.0d))
-    assert(Arbitrary.empty[Primitives] === Primitives(false, 0, 0, 0, 0L, 0.0f, 0.0d))
-    assert(Arbitrary.indexed[Primitives](0) === Primitives(false, 1, 2, 3, 4L, 5.0f, 6.0d))
-    assert(Arbitrary.random[Primitives](42) === expectedRandom42)
-    assert(Arbitrary.hashed[Primitives](0) === expectedHashed0)
-    assert(Arbitrary.hashed[Primitives](1) === expectedHashed1)
-
-    val hashedArbitrary = new Arbitrary(ArbitraryStrategy.HASHED, seed = 0)
-    assert(hashedArbitrary.make[Primitives] === expectedHashed0)
-    assert(hashedArbitrary.make[Primitives] === expectedHashed1)
-
-    val hashedConstantArbitrary = new Arbitrary(ArbitraryStrategy.HASHED_CONSTANT)
-    assert(hashedConstantArbitrary.make[Primitives] === expectedHashed0)
-    assert(hashedConstantArbitrary.make[Primitives] === expectedHashed0)
+    assert(constant[Primitives](42) === Primitives(false, 42, 42, 42, 42L, 42.0f, 42.0d))
+    assert(default[Primitives] === Primitives(false, 0, 0, 0, 0L, 0.0f, 0.0d))
+    assert(empty[Primitives] === Primitives(false, 0, 0, 0, 0L, 0.0f, 0.0d))
+    assert(indexed[Primitives](0) === Primitives(false, 1, 2, 3, 4L, 5.0f, 6.0d))
+    assert(hashed[Primitives](0) === expectedHashed0)
+    assert(hashed[Primitives](1) === expectedHashed1)
+    assert(proto[Primitives] === Primitives(false, 0, 0, 0, 0L, 0.0f, 0.0d))
+    assert(random[Primitives](42) === expectedRandom42)
 
     val indexedArbitrary = new Arbitrary(ArbitraryStrategy.INDEXED, seed = 0)
     assert(indexedArbitrary.make[Primitives] === Primitives(false, 1, 2, 3, 4L, 5.0f, 6.0d))
@@ -36,79 +33,120 @@ class ArbitraryTest extends AnyFunSuite {
     assert(indexedConstantArbitrary.make[Primitives] === Primitives(false, 0, 0, 0, 0L, 0.0f, 0.0d))
     assert(indexedConstantArbitrary.make[Primitives] === Primitives(true, 1, 1, 1, 1L, 1.0f, 1.0d))
 
+    val hashedArbitrary = new Arbitrary(ArbitraryStrategy.HASHED, seed = 0)
+    assert(hashedArbitrary.make[Primitives] === expectedHashed0)
+    assert(hashedArbitrary.make[Primitives] === expectedHashed1)
+
+    val hashedConstantArbitrary = new Arbitrary(ArbitraryStrategy.HASHED_CONSTANT)
+    assert(hashedConstantArbitrary.make[Primitives] === expectedHashed0)
+    assert(hashedConstantArbitrary.make[Primitives] === expectedHashed0)
+
     val randomArbitrary = new Arbitrary(ArbitraryStrategy.RANDOM, seed = 42)
     assert(randomArbitrary.make[Primitives] === expectedRandom42)
     assert(randomArbitrary.make[Primitives] !== expectedRandom42)
   }
 
   test("Characters") {
-    assert(Arbitrary.constant[Characters](42) === Characters(42.toChar, "42"))
-    assert(Arbitrary.empty[Characters] === Characters('\u0000', null))
-    assert(Arbitrary.indexed[Characters](0) === Characters('\u0000', "1"))
-    assert(Arbitrary.random[Characters](42) === Characters('鴵', "234785527"))
-    assert(Arbitrary.hashed[Characters](0) === Characters('ᮇ', "-1149263876"))
+    assert(constant[Characters](42) === Characters(42.toChar, "42"))
+    assert(default[Characters] === Characters('\u0000', null))
+    assert(empty[Characters] === Characters('\u0000', null))
+    assert(indexed[Characters](0) === Characters('\u0000', "1"))
+    assert(hashed[Characters](0) === Characters('钝', "-4936050758556305047"))
+    assert(proto[Characters] === Characters('\u0000', ""))
+    assert(random[Characters](42) === Characters('諷', "-5843495416241995736"))
   }
 
   test("Enumerations") {
-    assert(Arbitrary.empty[Enumerations] === Enumerations(ATEnumeration.FOO))
-    assert(Arbitrary.hashed[Enumerations](0) === Enumerations(ATEnumeration.BAR))
+    assert(default[Enumerations] === Enumerations(null))
+    assert(empty[Enumerations] === Enumerations(FooBarBaz.FOO))
+    assert(hashed[Enumerations](0) === Enumerations(FooBarBaz.BAZ))
+    assert(proto[Enumerations] === Enumerations(FooBarBaz.FOO))
   }
 
   test("Collections") {
-    val empty = Collections(Option.empty, Seq.empty, Set.empty, Map.empty)
-    assert(Arbitrary.constant[Collections](42) === empty)
-    assert(Arbitrary.empty[Collections] === empty)
-    assert(Arbitrary.indexed[Collections](0) === empty)
-    assert(Arbitrary.random[Collections](42) === empty)
-    assert(Arbitrary.hashed[Collections](0) === empty)
+    val emptyCollections = Collections(Option.empty, Seq.empty, Set.empty, Map.empty)
+    assert(constant[Collections](42) === emptyCollections)
+    assert(default[Collections] === Collections(null, null, null, null))
+    assert(empty[Collections] === emptyCollections)
+    assert(indexed[Collections](0) === emptyCollections)
+    assert(hashed[Collections](0) === emptyCollections)
+    assert(proto[Collections] === emptyCollections)
+    assert(random[Collections](42) === emptyCollections)
   }
 
   test("Nesting") {
-    assert(Arbitrary.constant[Nesting](42) === Nesting(Characters('*', "42")))
-    assert(Arbitrary.empty[Nesting] === Nesting(Characters('\u0000', null)))
-    assert(Arbitrary.indexed[Nesting](0) === Nesting(Characters('\u0001', "2")))
-    assert(Arbitrary.random[Nesting](42) === Nesting(Characters('諷', "-1360544799")))
-    assert(Arbitrary.hashed[Nesting](0) === Nesting(Characters('ᮇ', "-1149263876")))
+    assert(constant[Nesting](42) === Nesting(Characters('*', "42")))
+    assert(default[Nesting] === Nesting(Characters('\u0000', null)))
+    assert(empty[Nesting] === Nesting(Characters('\u0000', null)))
+    assert(indexed[Nesting](0) === Nesting(Characters('\u0001', "2")))
+    assert(hashed[Nesting](0) === Nesting(Characters('钝', "-4936050758556305047")))
+    assert(proto[Nesting] === Nesting(Characters('\u0000', "")))
+    assert(random[Nesting](42) === Nesting(Characters('쀨', "5694868678511409995")))
   }
 
   test("JavaTime") {
-    // TODO: JavaTime
+    assert(constant[JavaTime](42)
+        === javaTime("1970-02-12", "00:00:42", "1970-02-12T00:00:42", "1970-02-12T00:00:42Z"))
+    assert(default[JavaTime] === JavaTime(null, null, null, null))
+    assert(empty[JavaTime] === JavaTime(null, null, null, null))
+    assert(indexed[JavaTime](0) === javaTime("1970-01-01", "00:00:01", "1970-01-03T00:00:02", "1970-01-04T00:00:03Z"))
+    assert(hashed[JavaTime](0) === javaTime("7656-06-29", "03:28:23", "7550-05-19T13:10", "7101-03-03T22:18:05Z"))
+    assert(proto[JavaTime] === JavaTime(null, null, null, null))
+    assert(random[JavaTime](42) === javaTime("7452-04-03", "19:51:04", "6799-12-03T09:26:35", "1733-06-09T12:19:57Z"))
   }
 
   test("JavaSql") {
-    def date(s: String): java.sql.Date = java.sql.Date.valueOf(s)
-
-    def timestamp(s: String): java.sql.Timestamp = java.sql.Timestamp.valueOf(s)
-
-    assert(Arbitrary.constant[JavaSql](42) === JavaSql(date("1970-02-12"), timestamp("1970-02-12 00:00:42.0")))
-//    assert(Arbitrary.empty[JavaSql] === JavaSql(date("1970-01-01"), timestamp("1970-01-01 00:00:00.0")))
-    assert(Arbitrary.empty[JavaSql] === JavaSql(null, null))
-    assert(Arbitrary.indexed[JavaSql](0) === JavaSql(date("1970-01-01"), timestamp("1970-01-02 00:00:01.0")))
-    assert(Arbitrary.random[JavaSql](42) === JavaSql(date("5730-03-01"), timestamp("2391-03-22 10:57:24.0")))
-    assert(Arbitrary.hashed[JavaSql](0) === JavaSql(date("4964-10-05"), timestamp("5241-09-26 01:19:43.0")))
+    assert(constant[JavaSql](42) === javaSql("1970-02-12", "1970-02-12 00:00:42.0"))
+    assert(default[JavaSql] === JavaSql(null, null))
+    assert(empty[JavaSql] === JavaSql(null, null))
+    assert(indexed[JavaSql](0) === javaSql("1970-01-01", "1970-01-02 00:00:01.0"))
+    assert(hashed[JavaSql](0) === javaSql("9442-02-07", "3632-04-30 04:26:37.0"))
+    assert(proto[JavaSql] === JavaSql(null, null))
+    assert(random[JavaSql](42) === javaSql("7452-04-03", "8442-06-10 19:51:04.0"))
   }
 
-  test("Evolution") {
-    assert(Arbitrary.random[EvolutionV1](42) === EvolutionV1(-1170105035, 234785527))
-    assert(Arbitrary.random[EvolutionV2](42) === EvolutionV2(-1170105035, 234785527, -1360544799))
-    assert(Arbitrary.hashed[EvolutionV1](0) === EvolutionV1(-2093851344, -1424472218))
-    assert(Arbitrary.hashed[EvolutionV2](0) === EvolutionV2(1568424554, -1424472218, -625191634))
+  test("SchemaEvolution") {
+    assert(hashed[SchemaEvolutionV1](0) === SchemaEvolutionV1(-1467009098, 322257997))
+    assert(hashed[SchemaEvolutionV2](0) === SchemaEvolutionV2(29333682, -1467009098, 331146028))
+    assert(random[SchemaEvolutionV1](42) === SchemaEvolutionV1(234785527, 205897768))
+    assert(random[SchemaEvolutionV2](42) === SchemaEvolutionV2(234785527, 205897768, -248792245))
+  }
+
+  test("clamp") {
+    assert(clamp(-1, -1, 1) === -1)
+    assert(clamp(0, -1, 1) === 0)
+    assert(clamp(1, -1, 1) === 1)
+    assert(clamp(-2, -1, 1) === 1)
+    assert(clamp(2, -1, 1) === -1)
+    assert(clamp(-5, -1, 1) === 1)
+    assert(clamp(-42, -1, 1) === 0)
   }
 }
 
 object ArbitraryTest {
-  object ATEnumeration extends Enumeration {
+  object FooBarBaz extends Enumeration {
+    type FooBarBaz = Value
     val FOO, BAR, BAZ = Value
   }
 
   case class Primitives(boolean: Boolean, byte: Byte, short: Short, int: Int, long: Long, float: Float, double: Double)
   case class Characters(char: Char, string: String)
-  case class Enumerations(enumeration: ATEnumeration.Value) // TODO: sealed traits, java Enum
+  case class Enumerations(enumeration: FooBarBaz.Value) // TODO: sealed traits, java Enum
   case class Collections(option: Option[Int], seq: Seq[Int], set: Set[Int], map: Map[Int, Int])
   case class Nesting(characters: Characters)
+
   case class JavaTime(localDate: LocalDate, localTime: LocalTime, localDateTime: LocalDateTime, instant: Instant)
   case class JavaSql(date: java.sql.Date, timestamp: java.sql.Timestamp)
 
-  case class EvolutionV1(foo: Int, bar: Int)
-  case class EvolutionV2(renamed: Int, bar: Int, appended: Int)
+  case class SchemaEvolutionV1(foo: Int, bar: Int)
+  case class SchemaEvolutionV2(inserted: Int, foo: Int, appended: Int)
+
+  def javaTime(localDate: String, localTime: String, localDateTime: String, instant: String): JavaTime = {
+    JavaTime(LocalDate.parse(localDate), LocalTime.parse(localTime), LocalDateTime.parse(localDateTime),
+      Instant.parse(instant))
+  }
+
+  def javaSql(date: String, timestamp: String): JavaSql = {
+    JavaSql(java.sql.Date.valueOf(date), java.sql.Timestamp.valueOf(timestamp))
+  }
 }
