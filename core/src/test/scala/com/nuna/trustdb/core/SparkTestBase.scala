a5f2190873ba40aada08a8ddd1b8cd27939e7e45
Jan Prach <jendap@gmail.com>
2020-03-02 11:36:54 -0800

    improve SparkTestBase
    * add dfs
    * add createLocalTempDirectory
    * add readDataset/writeDataset and readData/writeData
    * move spark-warehouse to target (convenient for maven project)

--- core/src/test/scala/com/nuna/trustdb/core/SparkTestBase.scala
+++ core/src/test/scala/com/nuna/trustdb/core/SparkTestBase.scala
@@ -1,18 +1,24 @@
 package com.nuna.trustdb.core
 
+import java.nio.file.{Files, Path}
 import java.sql.Date
 
 import com.github.mrpowers.spark.fast.tests.DataFrameComparer
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import com.nuna.trustdb.core.spark.Dfs
+import org.apache.spark.sql._
 import org.scalatest.funsuite.AnyFunSuite
 
 class SparkTestBase extends AnyFunSuite {
   lazy val sparkSession = SparkSession.builder()
       .master("local[1]")
-      .config("spark.ui.disable", "true")
+      .config("spark.ui.enabled", "false")
+      .config("spark.master.rest.enabled", "false")
       .config("spark.sql.shuffle.partitions", "1")
-      .appName(this.getClass.getName)
+      .config("spark.sql.warehouse.dir", "target/spark-warehouse")
+      .appName(this.getClass.getName.stripSuffix("$"))
       .getOrCreate()
+
+  lazy val dfs = new Dfs(sparkSession)
 
   def assertDatasetEquality[T](actual: Dataset[T], expected: Dataset[T], ignoreNullable: Boolean = false,
       ignoreColumnNames: Boolean = false, ignoreOrdering: Boolean = true): Unit = {
@@ -29,6 +35,30 @@ class SparkTestBase extends AnyFunSuite {
       SparkTestBase.comparer.assertApproximateDataFrameEquality(actual, expected, precision = precision,
         ignoreNullable = ignoreNullable, ignoreColumnNames = ignoreColumnNames, orderedComparison = !ignoreOrdering)
     }
+  }
+
+  def createLocalTempDirectory(deleteOnExit: Boolean = true): Path = {
+    val directory = Files.createTempDirectory(this.getClass.getSimpleName + ".")
+    if (deleteOnExit) {
+      directory.toFile.deleteOnExit()
+    }
+    directory
+  }
+
+  def readDataset[T: Encoder](path: String): Dataset[T] = {
+    sparkSession.read.format("parquet").load(path).as[T]
+  }
+
+  def readData[T: Encoder](path: String): Seq[T] = {
+    readDataset[T](path).collect()
+  }
+
+  def writeDataset[T: Encoder](path: String, dataset: Dataset[T]): Unit = {
+    dataset.write.format("parquet").mode(SaveMode.Overwrite).save(path)
+  }
+
+  def writeData[T: Encoder](path: String, data: Seq[T]): Unit = {
+    writeDataset[T](path, sparkSession.createDataset(data))
   }
 }
 
