96d358bde31f1d065e3ad2950e30a9abc07ae005
Jan Prach <jendap@gmail.com>
2020-09-30 03:02:16 +0200

    add Datasets.asDS

--- core/src/main/scala/com/nuna/trustdb/core/spark/Datasets.scala
+++ core/src/main/scala/com/nuna/trustdb/core/spark/Datasets.scala
@@ -1,9 +1,13 @@
 package com.nuna.trustdb.core.spark
 
-import com.nuna.trustdb.core.util.Reflection
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.{DataFrame, Dataset}
+import com.nuna.trustdb.core.util.{Reflection, Strings}
+import org.apache.spark.sql.catalyst.ScalaReflection
+import org.apache.spark.sql.catalyst.expressions.{Cast, CreateArray, CreateMap, Expression, Literal}
+import org.apache.spark.sql.functions.{array, coalesce, col, struct}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Column, DataFrame, Dataset}
 
+import scala.collection.mutable
 import scala.reflect.runtime.universe.TypeTag
 
 object Datasets {
@@ -41,9 +45,285 @@ object Datasets {
     }
   }
 
+  case class Config(
+      /**
+       * Coerce types. It will automatically do the usual upcasting like [[Int]] to [[Long]] but not the other way
+       * around (as it would loose precision). Beware of strings though! Everything can be casted implicitly
+       * to [[String]] but it may not be what one intends to do.
+       */
+      coerceTypes: Boolean,
+      /**
+       * NOT IMPLEMENTED YET!!! It throws [[Config.ConflictResolution.ERROR]] regardless of the config (for now).
+       *
+       * What should happen when two conflicting column definitions are encountered? In particular the value
+       * [[Config.ConflictResolution.LAST]] is useful. It solves the common pattern of conflicting columns occurring
+       * in queries like "SELECT *, "some_value" AS value FROM table" when "table" already has "value".
+       */
+      conflictResolution: Config.ConflictResolution.ConflictingNameResolution,
+      /**
+       * Should we fail if we drop extra column (not present in target schema)?
+       */
+      failOnDroppingExtraColumn: Boolean,
+      /**
+       * Should we fill in default values instead of nulls for non nullable columns?
+       *
+       * Default values are 0 for numbers, false for [[Boolean]], "" for [[String]], empty arrays and nested struct
+       * are present filled with the previous rules.
+       *
+       * Note: It probably does not make sense to use this without also setting fillMissingNulls = true.
+       *
+       * BEWARE: This is not intended for production code! Filling blindly all columns with default values defeats
+       * the purpose of why they are not defined nullable in the first place. It would be error prone to use this.
+       * But it may come handy in some notebook exploration or unit tests.
+       */
+      fillDefaultValues: Boolean,
+      /**
+       * Should we fill nulls instead of missing nullable columns?
+       */
+      fillMissingNulls: Boolean,
+      /**
+       * Should names be normalized before matching?
+       *
+       * Number of normalizations happen - lowercase, replace non-word characters with underscores, etc. For details
+       * see [[Strings.toSnakeCase]].
+       */
+      normalizedNameMatching: Boolean,
+      /**
+       * How should the output columns be sorted? Use [[Config.SortOrder.TARGET]] to get the order of target schema.
+       */
+      sortOrder: Config.SortOrder.SortOrder,
+      /**
+       * Should we remove the extra columns (not present in target schema)? Use false to keep ghe extra columns
+       * (just like [[Dataset.as]] does).
+       */
+      removeExtraColumns: Boolean,
+  )
+
+  object Config {
+    object ConflictResolution extends Enumeration {
+      type ConflictingNameResolution = Value
+      val UNDEFINED, ERROR, FIRST, LAST = Value
+    }
+
+    object SortOrder extends Enumeration {
+      type SortOrder = Value
+      val UNDEFINED, ALPHABETICAL, SOURCE, TARGET = Value
+    }
+
+    val safe = Config(
+      coerceTypes = false,
+      conflictResolution = Config.ConflictResolution.ERROR,
+      failOnDroppingExtraColumn = true,
+      fillDefaultValues = false,
+      fillMissingNulls = false,
+      normalizedNameMatching = false,
+      sortOrder = Config.SortOrder.SOURCE,
+      removeExtraColumns = false,
+    )
+    val default = safe.copy(
+      coerceTypes = true,
+      failOnDroppingExtraColumn = false,
+      sortOrder = Config.SortOrder.TARGET,
+      removeExtraColumns = true,
+    )
+    val powerful = default.copy(
+      conflictResolution = Config.ConflictResolution.LAST,
+      fillMissingNulls = true,
+      normalizedNameMatching = true,
+    )
+    val dangerous = powerful.copy(
+      fillDefaultValues = true,
+    )
+  }
+
+  case class StructValidationError(
+      path: String,
+      msg: String,
+  )
+
+  class StructValidationException(val errors: Seq[StructValidationError]) extends Exception {
+    override def getMessage: String = {
+      val errorStrings = errors.map(e => s"Column ${e.path} ${e.msg}")
+      s"Errors: [${errorStrings.mkString("\n    - ", "\n    - ", "\n")}]"
+    }
+  }
+
+  private def defaultValueExpression(dataType: DataType): Expression = {
+    dataType match {
+      case ArrayType(internalType: DataType, _) =>
+        CreateArray(Seq(Literal.default(internalType)))
+      case MapType(keyDataType: DataType, valueDataType: DataType, _) =>
+        CreateMap(Seq(Literal.default(keyDataType), Literal.default(valueDataType)))
+      case _ => Literal.default(dataType)
+    }
+  }
+
+  private def nullValueExpression(dataType: DataType): Expression = {
+    dataType match {
+      case ArrayType(internalType: DataType, _) =>
+        CreateArray(Seq(Literal.create(null, internalType)))
+      case MapType(keyDataType: DataType, valueDataType: DataType, _) =>
+        CreateMap(Seq(Literal.create(null, keyDataType), Literal.create(null, valueDataType)))
+      case _ => Literal.create(null, dataType)
+    }
+  }
+
+  private def makeFiledPath(path: String, fieldName: String): String = {
+    Seq(path, fieldName).filter(_ != null).mkString(".")
+  }
+
+  private def coarseStructField(source: StructField, target: StructField, config: Config,
+      path: String): Either[List[StructValidationError], List[Column]] = {
+    val fieldPath = makeFiledPath(path, target.name)
+
+    val errors = mutable.Buffer.empty[StructValidationError]
+    var column = col(makeFiledPath(path, source.name))
+    if (source.nullable && !target.nullable) {
+      if (config.fillDefaultValues) {
+        column = coalesce(column, new Column(defaultValueExpression(target.dataType)))
+      } else {
+        errors += StructValidationError(fieldPath, "is nullable but target column is not")
+      }
+    }
+    (source.dataType, target.dataType) match {
+      case (sourceDataType, targetDataType) if sourceDataType == targetDataType => // pass
+      case (sourceStructType: StructType, targetStructType: StructType) =>
+        coarseStructType(sourceStructType, targetStructType, config, fieldPath) match {
+          case Right(nestedColumns) => column = struct(nestedColumns: _*)
+          case Left(nestedErrors) => errors ++= nestedErrors
+        }
+      case (_: StructType, targetDataType) =>
+        errors += StructValidationError(fieldPath, s"struct cannot be casted to target type $targetDataType")
+      case (sourceDataType, _: StructType) if !sourceDataType.isInstanceOf[NullType] =>
+        errors += StructValidationError(fieldPath, s"source type $sourceDataType cannot be casted to struct")
+      case (sourceArrayType: ArrayType, targetArrayType: ArrayType) =>
+        val sourceFieldType = StructField(null, sourceArrayType.elementType)
+        val targetFieldType = StructField(null, targetArrayType.elementType)
+        coarseStructField(sourceFieldType, targetFieldType, config, fieldPath) match {
+          case Right(nestedColumns) =>
+            assert(nestedColumns.size == 1)
+            column = array(nestedColumns: _*)
+          case Left(nestedErrors) =>
+            errors ++= nestedErrors
+        }
+      case (_: ArrayType, targetDataType) =>
+        errors += StructValidationError(fieldPath, s"array cannot be casted to target type $targetDataType")
+      case (sourceDataType, _: ArrayType) if !sourceDataType.isInstanceOf[NullType] =>
+        errors += StructValidationError(fieldPath, s"source type $sourceDataType cannot be casted to array")
+      case (sourceDataType, targetDataType) =>
+        if (Cast.canCast(sourceDataType, targetDataType) && config.coerceTypes) {
+          column = column.cast(targetDataType)
+        } else {
+          errors += StructValidationError(fieldPath,
+            s"type $sourceDataType cannot be casted to target type $targetDataType")
+        }
+    }
+
+    if (errors.isEmpty) {
+      Right(List(Option(target.name).map(tn => column.as(tn)).getOrElse(column)))
+    } else {
+      Left(errors.toList)
+    }
+  }
+
+  private def coarseStructType(sourceStruct: StructType, targetStruct: StructType, config: Config,
+      path: String = null): Either[List[StructValidationError], List[Column]] = {
+    def normalizedName(field: StructField): String = {
+      // TODO: normalize even more? (leading and trailing spaces, etc)
+      if (config.normalizedNameMatching) Strings.toSnakeCase(field.name) else field.name
+    }
+
+    val sourceFieldsByName = sourceStruct.fields.toList.groupBy(normalizedName)
+    val targetFieldsByName = targetStruct.fields.toList.groupBy(normalizedName)
+
+    val fieldNames = config.sortOrder match {
+      case Config.SortOrder.ALPHABETICAL => (sourceFieldsByName.keys ++ targetFieldsByName.keys).toSeq.sorted
+      case Config.SortOrder.SOURCE => (sourceStruct.fields ++ targetStruct.fields).map(normalizedName).toSeq.distinct
+      case Config.SortOrder.TARGET | Config.SortOrder.UNDEFINED =>
+        (targetStruct.fields ++ sourceStruct.fields).map(normalizedName).toSeq.distinct
+    }
+
+    val resultColumns = fieldNames.map { fieldName =>
+      val fieldPath = makeFiledPath(path, fieldName)
+      (sourceFieldsByName.getOrElse(fieldName, List.empty), targetFieldsByName.getOrElse(fieldName, List.empty)) match {
+        case (Nil, target :: Nil) =>
+          if (target.nullable && config.fillMissingNulls) {
+            Right(List(new Column(nullValueExpression(target.dataType)).name(target.name)))
+          } else if (config.fillDefaultValues) {
+            Right(List(new Column(defaultValueExpression(target.dataType)).as(target.name)))
+          } else {
+            Left(List(StructValidationError(fieldPath, "is missing")))
+          }
+        case (source :: Nil, target :: Nil) =>
+          coarseStructField(source, target, config, path)
+        case (sources, target :: Nil) =>
+          // TODO: ConflictResolution is more complicated than this
+//          config.conflictResolution match {
+//            case ConflictResolution.ERROR | ConflictResolution.UNDEFINED =>
+//              Left(List(StructValidationError(fieldPath, s"has ${sources.size} conflicting occurrences")))
+//            case ConflictResolution.FIRST => coarseStructField(sources.head, target, config, path)
+//            case ConflictResolution.LAST => coarseStructField(sources.last, target, config, path)
+//          }
+          Left(List(StructValidationError(fieldPath, s"has ${sources.size} conflicting occurrences")))
+        case (sources, Nil) =>
+          if (config.failOnDroppingExtraColumn) {
+            Left(List(StructValidationError(fieldPath, s"has been dropped (${sources.size}x)")))
+          } else {
+            if (config.removeExtraColumns) {
+              Right(List.empty)
+            } else {
+              Right(List(col(fieldPath)))
+            }
+          }
+        case (_, targets) if targets.size > 1 =>
+          Left(List(StructValidationError(fieldPath, s"has ${targets.size} occurrences in target struct")))
+      }
+    }
+
+    resultColumns.toList.partition(_.isLeft) match {
+      case (Nil, columns) => Right(columns.flatMap(_.toOption).flatten)
+      case (errors, _) => Left(errors.flatMap(_.left.toOption).flatten)
+    }
+  }
+
+  /**
+   * [[asDS]][T] is similar to spark's own [[Dataset.as]][T] but way more powerful and configurable
+   *
+   * See [[Config]] for supported configuration and/or unit tests for examples. Just be aware that the same config
+   * is applied on all the columns (even nested).
+   *
+   * @param ds input [[Dataset]] or [[DataFrame]]
+   * @param config configuration - see [[Config]] for details
+   * @tparam T case class to which we are casting the df
+   * @return [[Dataset]][T] with only the columns present in case class and in that order
+   */
+  def asDS[T <: Product : TypeTag](ds: Dataset[_], config: Config = Config.default): Dataset[T] = {
+    import ds.sparkSession.implicits._
+
+    val actualSchema = ds.schema
+    val expectedSchema = ScalaReflection.schemaFor[T].dataType.asInstanceOf[StructType]
+
+    if (actualSchema == expectedSchema) {
+      ds.as[T]
+    } else {
+      coarseStructType(actualSchema, expectedSchema, config) match {
+        case Left(errors) => throw new StructValidationException(errors)
+        case Right(columns) => ds.select(columns: _*).as[T]
+      }
+    }
+  }
+
   class RichDataset(val ds: Dataset[_]) extends AnyVal {
     def asDataset[T <: Product : TypeTag]: Dataset[T] = {
       Datasets.asDataset[T](ds)
+    }
+
+    def asDS[T <: Product : TypeTag]: Dataset[T] = {
+      Datasets.asDS[T](ds)
+    }
+
+    def asDS[T <: Product : TypeTag](config: Config): Dataset[T] = {
+      Datasets.asDS[T](ds, config)
     }
   }
 }
--- core/src/test/scala/com/nuna/trustdb/core/spark/DatasetsTest.scala
+++ core/src/test/scala/com/nuna/trustdb/core/spark/DatasetsTest.scala
@@ -1,44 +1,132 @@
 package com.nuna.trustdb.core.spark
 
 import com.nuna.trustdb.core.SparkTestBase
+import com.nuna.trustdb.core.spark.Datasets.StructValidationException
 
 class DatasetsTest extends SparkTestBase {
   import DatasetsTest._
   import sparkSession.implicits._
 
-  lazy val records = Seq(Record("foo", 42, NestedRecord("nested"))).toDF()
+  val records = Seq(Record("foo", 42, NestedRecord("nested")))
+  lazy val recordsDF = records.toDF()
 
-  test("projection") {
-    val subsetOfRecords = Datasets.asDataset[SubsetOfRecord](records)
+  test("asDataset - projection") {
+    val subsetOfRecords = Datasets.asDataset[SubsetOfRecord](recordsDF)
     assert(subsetOfRecords.columns === Seq("str", "nested"))
   }
 
-  test("reordering") {
-    val recordsWithReorderedColumns = records.select("num", "str", "nested")
+  test("asDataset - reordering") {
+    val recordsWithReorderedColumns = recordsDF.select("num", "str", "nested")
     assert(recordsWithReorderedColumns.columns === Seq("num", "str", "nested"))
     val recordsWithProperlyReorderedColumns = Datasets.asDataset[Record](recordsWithReorderedColumns)
     assert(recordsWithProperlyReorderedColumns.columns === Seq("str", "num", "nested"))
   }
 
-  test("missing columns") {
-    val caught = intercept[RuntimeException](Datasets.asDataset[NestedRecord](records))
+  test("asDataset - missing columns") {
+    val caught = intercept[RuntimeException](Datasets.asDataset[NestedRecord](recordsDF))
     assert(caught.getMessage.contains("missing columns"))
     assert(caught.getMessage.contains("nestedStr"))
   }
 
-  test("duplicate columns") {
-    val recordsWithDuplicateColumns = records.select("str", "str", "nested")
+  test("asDataset - duplicate columns") {
+    val recordsWithDuplicateColumns = recordsDF.select("str", "str", "nested")
     val caught = intercept[RuntimeException](Datasets.asDataset[SubsetOfRecord](recordsWithDuplicateColumns))
     assert(caught.getMessage.contains("duplicate columns"))
     assert(caught.getMessage.contains("str"))
   }
 
-  test("implicits") {
+  test("asDataset - implicits") {
     import com.nuna.trustdb.core.spark.implicits._
-    val recordsWithReorderedColumns = records.select("num", "str", "nested")
+    val recordsWithReorderedColumns = recordsDF.select("num", "str", "nested")
     assert(recordsWithReorderedColumns.asDataset[SubsetOfRecord].columns === Seq("str", "nested"))
     val recordsWithReorderedColumnsDS = recordsWithReorderedColumns.as[SubsetOfRecord]
     assert(recordsWithReorderedColumnsDS.asDataset[SubsetOfRecord].columns === Seq("str", "nested"))
+  }
+
+  test("asDS - coerceTypes") {
+    val fixableDF = recordsDF.selectExpr("42 AS str", "CAST(num AS byte) AS num", "nested")
+    val fixedDS = Datasets.asDS[Record](fixableDF)
+    assert(fixedDS.collect() === Seq(records.head.copy(str = "42")))
+    val config = Datasets.Config.default.copy(coerceTypes = false)
+    val caught = intercept[StructValidationException](Datasets.asDS[Record](fixableDF, config))
+    assert(caught.getMessage.contains("str type IntegerType cannot be casted to target type StringType"))
+    assert(caught.getMessage.contains("num type ByteType cannot be casted to target type IntegerType"))
+  }
+
+  ignore("asDS - conflictResolution") {
+    ??? // TODO: implement conflict resolution
+  }
+
+  test("asDS - failOnDroppingExtraColumn") {
+    val fixableDF = recordsDF.selectExpr("*", "'dropped_value' AS dropped_column")
+    val fixedDS = Datasets.asDS[Record](fixableDF)
+    assert(fixedDS.collect() === records)
+    val config = Datasets.Config.default.copy(failOnDroppingExtraColumn = true)
+    val caught = intercept[StructValidationException](Datasets.asDS[Record](fixableDF, config))
+    assert(caught.getMessage.contains("dropped_column has been dropped"))
+  }
+
+  test("asDS - fillDefaultValues") {
+    val fixableDF = sparkSession.sql("SELECT 'unused' AS dummy_column")
+    val caught = intercept[StructValidationException](Datasets.asDS[Record](fixableDF))
+    assert(caught.getMessage.contains("str is missing"))
+    assert(caught.getMessage.contains("num is missing"))
+    assert(caught.getMessage.contains("nested is missing"))
+    val config = Datasets.Config.default.copy(fillDefaultValues = true)
+    val fixedDS = Datasets.asDS[Record](fixableDF, config)
+    assert(fixedDS.collect() === Array(Record("", 0, NestedRecord(""))))
+  }
+
+  test("asDS - fillMissingNulls") {
+    val fixableDF = sparkSession.sql("SELECT 42 AS num")
+    val caught = intercept[StructValidationException](Datasets.asDS[Record](fixableDF))
+    assert(caught.getMessage.contains("str is missing"))
+    assert(caught.getMessage.contains("nested is missing"))
+    val config = Datasets.Config.default.copy(fillMissingNulls = true)
+    val fixedDS = Datasets.asDS[Record](fixableDF, config)
+    assert(fixedDS.collect() === Array(Record(null, 42, null)))
+  }
+
+  test("asDS - normalizedNameMatching") {
+    val fixableDF = sparkSession.sql("SELECT 'foo' AS `nested str`")
+    val caught = intercept[StructValidationException](Datasets.asDS[NestedRecord](fixableDF))
+    assert(caught.getMessage.contains("nestedStr is missing"))
+    val config = Datasets.Config.default.copy(normalizedNameMatching = true)
+    val fixedDS = Datasets.asDS[NestedRecord](fixableDF, config)
+    assert(fixedDS.collect() === Array(NestedRecord("foo")))
+  }
+
+  test("asDS - sortOrder") {
+    val fixableDF = recordsDF.selectExpr("num", "nested", "str")
+    assert(Datasets.asDS[Record](fixableDF).columns === Seq("str", "num", "nested"))
+    val sourceOrderConfig = Datasets.Config.default.copy(sortOrder = Datasets.Config.SortOrder.SOURCE)
+    assert(Datasets.asDS[Record](fixableDF, sourceOrderConfig).columns === Seq("num", "nested", "str"))
+    val alphabeticalOrderConfig = Datasets.Config.default.copy(sortOrder = Datasets.Config.SortOrder.ALPHABETICAL)
+    assert(Datasets.asDS[Record](fixableDF, alphabeticalOrderConfig).columns === Seq("nested", "num", "str"))
+  }
+
+  test("asDS - removeExtraColumns") {
+    val df = recordsDF.selectExpr("*", "'foo' AS extra_column")
+    val dsWithoutExtraColumns = Datasets.asDS[Record](df)
+    assert(dsWithoutExtraColumns.columns === Seq("str", "num", "nested"))
+    val config = Datasets.Config.default.copy(removeExtraColumns = false)
+    val dsWithExtraColumns = Datasets.asDS[Record](df, config)
+    assert(dsWithExtraColumns.columns === Seq("str", "num", "nested", "extra_column"))
+  }
+
+  test("asDS - arrays") {
+    val df = sparkSession.sql("SELECT array(struct('str', 'foo')) AS records")
+    val config = Datasets.Config.dangerous.copy(sortOrder = Datasets.Config.SortOrder.ALPHABETICAL)
+    val ds = Datasets.asDS[SeqOfRecord](df, config)
+    assert(ds.collect() === Seq(SeqOfRecord(Seq(Record(null, 0, null)))))
+  }
+
+  test("asDS - implicits") {
+    import com.nuna.trustdb.core.spark.implicits._
+    val df = recordsDF.selectExpr("str", "struct('foo', 'nested str') AS nested", "'foo' AS extra_column")
+    val config = Datasets.Config.dangerous.copy(sortOrder = Datasets.Config.SortOrder.ALPHABETICAL)
+    val ds = df.asDS[Record](config)
+    assert(ds.columns === Seq("nested", "num", "str"))
   }
 }
 
@@ -46,4 +134,5 @@ object DatasetsTest {
   case class NestedRecord(nestedStr: String)
   case class Record(str: String, num: Int, nested: NestedRecord)
   case class SubsetOfRecord(str: String, nested: NestedRecord)
+  case class SeqOfRecord(records: Seq[Record])
 }
