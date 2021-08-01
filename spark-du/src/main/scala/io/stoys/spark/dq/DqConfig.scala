da2c734beb00c69c674bee0b868a4678905ea08f
Jan Prach <jendap@gmail.com>
2021-08-01 15:00:31 +0200

    add scaladoc for DqConfig

--- spark-du/src/main/scala/io/stoys/spark/dq/DqConfig.scala
+++ spark-du/src/main/scala/io/stoys/spark/dq/DqConfig.scala
@@ -1,9 +1,28 @@
 package io.stoys.spark.dq
 
 case class DqConfig(
+    /**
+     * Should [[DqResult.row_sample]] output be enabled?
+     */
     sample_rows: Boolean,
+
+    /**
+     * How many rows (at most) are sampled per rule?
+     *
+     * Note: The actual limit may be proportionally lowered if max_rows limit is reached.
+     */
     max_rows_per_rule: Int,
+
+    /**
+     * How many rows are sampled across all rules?
+     */
     max_rows: Int,
+
+    /**
+     * Should [[DqSchema]] generate rule failing (and listing) extra columns?
+     *
+     * Extra columns are silently ignored if this disabled.
+     */
     fail_on_extra_columns: Boolean
 )
 
