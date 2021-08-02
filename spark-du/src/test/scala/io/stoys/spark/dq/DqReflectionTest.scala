5563dbecd72c45e4b573bc67fb48dc583f31bd88
Jan Prach <jendap@gmail.com>
2021-08-02 11:24:17 +0200

    cleanup

--- spark-du/src/test/scala/io/stoys/spark/dq/DqReflectionTest.scala
+++ spark-du/src/test/scala/io/stoys/spark/dq/DqReflectionTest.scala
@@ -1,59 +1,55 @@
 package io.stoys.spark.dq
 
-import io.stoys.scala.Arbitrary
 import io.stoys.spark.SToysException
 import org.scalatest.funsuite.AnyFunSuite
 
 import java.sql.Date
 
 class DqReflectionTest extends AnyFunSuite {
+  import DqRules._
   import DqReflection._
   import DqReflectionTest._
 
-  test("basics") {
+  test("getDqFields - Record") {
     val expectedFields = Seq(
-      DqField("id", "\"integer\"", nullable = true, Seq.empty, None, None),
-      DqField("custom_date", "\"date\"", nullable = true, Seq.empty, Some("MM/dd/yyyy"), None),
-      DqField("custom_enum", "\"string\"", nullable = false, Seq("foo", "bar", "baz"), None, None)
+      field("i", "\"integer\""),
+      field("d", "\"date\"", format = "MM/dd/yyyy"),
+      field("e", "\"string\"", enumValues = Seq("foo", "bar", "baz"))
     )
     assert(getDqFields[Record] === expectedFields)
   }
 
-  test("nullability") {
-    val emptyDqField = Arbitrary.empty[DqField]
+  test("getDqFields - NullableRecord") {
     val expectedFields = Seq(
-      emptyDqField.copy(name = "int", data_type_json = "\"integer\"", nullable = true),
-      emptyDqField.copy(name = "str", data_type_json = "\"string\"", nullable = true),
-      emptyDqField.copy(name = "option_str", data_type_json = "\"string\"", nullable = true),
-      emptyDqField.copy(name = "dq_field_not_nullable_int", data_type_json = "\"integer\"", nullable = false),
-      emptyDqField.copy(name = "dq_field_not_nullable_str", data_type_json = "\"string\"", nullable = false),
-      emptyDqField.copy(name = "dq_field_not_nullable_option_str", data_type_json = "\"string\"", nullable = false)
+      field("i", "\"integer\""),
+      field("s", "\"string\""),
+      field("s_option", "\"string\""),
+      field("i__nullable", "\"integer\""),
+      field("i__not_nullable", "\"integer\"", nullable = false),
+      field("s__not_nullable", "\"string\"", nullable = false),
+      field("s_option__not_nullable", "\"string\"", nullable = false)
     )
     assert(getDqFields[NullableRecord] === expectedFields)
   }
 
-  test("collections") {
-    val emptyDqField = Arbitrary.empty[DqField]
-    val intSeqDataTypeJson = """{"type":"array","elementType":"integer","containsNull":false}"""
-    val strMapDataTypeJson = """{"type":"map","keyType":"string","valueType":"string","valueContainsNull":true}"""
+  test("getDqFields - CollectionRecord") {
     val expectedFields = Seq(
-      emptyDqField.copy(name = "int_seq", data_type_json = intSeqDataTypeJson, nullable = true),
-      emptyDqField.copy(name = "str_map", data_type_json = strMapDataTypeJson, nullable = true)
+      field("i_seq", """{"type":"array","elementType":"integer","containsNull":false}"""),
+      field("s_map", """{"type":"map","keyType":"string","valueType":"string","valueContainsNull":true}""")
     )
     assert(getDqFields[CollectionRecord] === expectedFields)
   }
 
-  test("poorly named") {
-    val emptyDqField = Arbitrary.empty[DqField]
+  test("getDqFields - PoorlyNamedRecord") {
     val expectedFields = Seq(
-      emptyDqField.copy(name = "s +", data_type_json = "\"string\"", nullable = true),
-      emptyDqField.copy(name = "s -", data_type_json = "\"string\"", nullable = true),
-      emptyDqField.copy(name = "mixed_CASE", data_type_json = "\"string\"", nullable = true),
+      field("s +", "\"string\""),
+      field("s -", "\"string\""),
+      field("mixed_CASE", "\"string\"")
     )
     assert(getDqFields[PoorlyNamedRecord] === expectedFields)
   }
 
-  test("unsupported") {
+  test("getDqFields - UnsupportedRecord") {
     assertThrows[SToysException](getDqFields[UnsupportedRecord])
     assert(getDqFields[UnsupportedRecord](ignoreUnsupportedTypes = true) === Seq.empty)
   }
@@ -63,30 +59,32 @@ object DqReflectionTest {
   import annotation.DqField
 
   case class Record(
-      id: Int,
-      @DqField(nullable = true, format = "MM/dd/yyyy")
-      custom_date: Date,
-      @DqField(nullable = false, enumValues = Array("foo", "bar", "baz"))
-      custom_enum: String,
+      i: Int,
+      @DqField(format = "MM/dd/yyyy")
+      d: Date,
+      @DqField(enumValues = Array("foo", "bar", "baz"))
+      e: String,
       @DqField(ignore = true)
       ignored: String
   )
 
   case class NullableRecord(
-      int: Int,
-      str: String,
-      option_str: Option[String],
+      i: Int,
+      s: String,
+      s_option: Option[String],
+      @DqField(nullable = true)
+      i__nullable: Int,
       @DqField(nullable = false)
-      dq_field_not_nullable_int: Int,
+      i__not_nullable: Int,
       @DqField(nullable = false)
-      dq_field_not_nullable_str: String,
+      s__not_nullable: String,
       @DqField(nullable = false)
-      dq_field_not_nullable_option_str: Option[String]
+      s_option__not_nullable: Option[String]
   )
 
   case class CollectionRecord(
-      int_seq: Seq[Int],
-      str_map: Map[String, String]
+      i_seq: Seq[Int],
+      s_map: Map[String, String]
   )
 
   case class PoorlyNamedRecord(
